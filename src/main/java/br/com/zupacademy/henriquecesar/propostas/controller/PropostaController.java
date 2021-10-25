2e466c3027f2065c0856a1850cf4f38e4e885cc5
Henrique César <henrique.jose@zup.com.br>
2021-10-25 07:50:32 -0300

    criação do endpoint de cadastro de proposta

--- src/main/java/br/com/zupacademy/henriquecesar/propostas/controller/PropostaController.java
+++ src/main/java/br/com/zupacademy/henriquecesar/propostas/controller/PropostaController.java
@@ -1,10 +1,40 @@
 package br.com.zupacademy.henriquecesar.propostas.controller;
 
+import java.net.URI;
+
+import javax.validation.Valid;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import br.com.zupacademy.henriquecesar.propostas.dto.request.NovaPropostaRequest;
+import br.com.zupacademy.henriquecesar.propostas.modelo.Proposta;
+import br.com.zupacademy.henriquecesar.propostas.repository.PropostaRepository;
 
 @RestController
 @RequestMapping("/propostas")
 public class PropostaController {
+	
+	private PropostaRepository propostaRepository;
+	
+	public PropostaController(PropostaRepository propostaRepository) {
+		this.propostaRepository = propostaRepository;
+	}
+	
+	@PostMapping
+	public ResponseEntity<?> criarProposta(@RequestBody @Valid NovaPropostaRequest request,
+			UriComponentsBuilder uriBuilder) {
+		Proposta proposta = Proposta.buildProposta(request);
+		propostaRepository.save(proposta);
+		
+		URI location = uriBuilder.replacePath("/propostas/{id}")
+				.buildAndExpand(proposta.getId()).toUri();
+		
+		return ResponseEntity.created(location).build();
+	}
 
 }
