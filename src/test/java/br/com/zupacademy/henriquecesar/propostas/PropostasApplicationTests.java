dcec7ee4b99b139caaf4e09dcf5d838e8bf78a52
Henrique César <henrique.jose@zup.com.br>
2021-10-25 13:40:30 -0300

    remove @EnableFeignClients da classe PropostasApplicationTests

--- src/test/java/br/com/zupacademy/henriquecesar/propostas/PropostasApplicationTests.java
+++ src/test/java/br/com/zupacademy/henriquecesar/propostas/PropostasApplicationTests.java
@@ -2,10 +2,8 @@ package br.com.zupacademy.henriquecesar.propostas;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootTest
-@EnableFeignClients
 class PropostasApplicationTests {
 
 	@Test
