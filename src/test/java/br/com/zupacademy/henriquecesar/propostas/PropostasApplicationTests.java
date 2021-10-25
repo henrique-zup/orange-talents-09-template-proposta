1e78b3bd47b8963e4e5a478599afc774fc305e9b
Henrique César <henrique.jose@zup.com.br>
2021-10-25 07:16:57 -0300

    commit estrutural

---
+++ src/test/java/br/com/zupacademy/henriquecesar/propostas/PropostasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.henriquecesar.propostas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropostasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
