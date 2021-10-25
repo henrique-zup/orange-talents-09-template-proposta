1d836ce8427259fb65974a08bffed671c81a4216
Henrique César <henrique.jose@zup.com.br>
2021-10-25 07:50:02 -0300

    criação do PropostaController

---
+++ src/main/java/br/com/zupacademy/henriquecesar/propostas/controller/PropostaController.java
@@ -0,0 +1,10 @@
+package br.com.zupacademy.henriquecesar.propostas.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/propostas")
+public class PropostaController {
+
+}
