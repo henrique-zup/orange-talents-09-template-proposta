3b2ad391064907d3cd5720784db4a14adeb8d4d1
Henrique César <henrique.jose@zup.com.br>
2021-10-25 09:00:25 -0300

    adição de método para buscar proposta por documento

--- src/main/java/br/com/zupacademy/henriquecesar/propostas/repository/PropostaRepository.java
+++ src/main/java/br/com/zupacademy/henriquecesar/propostas/repository/PropostaRepository.java
@@ -1,9 +1,13 @@
 package br.com.zupacademy.henriquecesar.propostas.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import br.com.zupacademy.henriquecesar.propostas.modelo.Proposta;
 
 public interface PropostaRepository extends CrudRepository<Proposta, Long>{
+
+    Optional<Proposta> findByDocumento(String documento);
 
 }
