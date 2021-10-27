7bf6a8c7552a051e34af0dd7276394e4e1b03bf9
Henrique César <henrique.jose@zup.com.br>
2021-10-27 16:58:45 -0300

    adição de método para retornar propostas elegíveis sem cartão

--- src/main/java/br/com/zupacademy/henriquecesar/propostas/repository/PropostaRepository.java
+++ src/main/java/br/com/zupacademy/henriquecesar/propostas/repository/PropostaRepository.java
@@ -1,13 +1,18 @@
 package br.com.zupacademy.henriquecesar.propostas.repository;
 
+import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
 import br.com.zupacademy.henriquecesar.propostas.modelo.Proposta;
+import br.com.zupacademy.henriquecesar.propostas.modelo.PropostaStatus;
 
 public interface PropostaRepository extends CrudRepository<Proposta, Long>{
 
     Optional<Proposta> findByDocumento(String documento);
+
+	List<Proposta> findByCartaoIsNullAndStatusEquals(PropostaStatus status, Pageable pageRequest);
 
 }
