57cac6c0888235e9366dc15e9fcffa91670ae903
Henrique César <henrique.jose@zup.com.br>
2021-10-27 17:28:12 -0300

    criação da classe PropostaStatusResponse

---
+++ src/main/java/br/com/zupacademy/henriquecesar/propostas/dto/response/PropostaStatusResponse.java
@@ -0,0 +1,22 @@
+package br.com.zupacademy.henriquecesar.propostas.dto.response;
+
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+
+import br.com.zupacademy.henriquecesar.propostas.modelo.Proposta;
+import br.com.zupacademy.henriquecesar.propostas.modelo.PropostaStatus;
+
+public class PropostaStatusResponse {
+	
+	@Enumerated(EnumType.STRING)
+	private PropostaStatus status;
+
+	public PropostaStatus getStatus() {
+		return status;
+	}
+	
+	public PropostaStatusResponse(Proposta proposta) {
+		status = proposta.getStatus();
+	}
+
+}
