4ca025557b53549f790a017d11cc97c01434814f
Henrique César <henrique.jose@zup.com.br>
2021-10-29 15:30:36 -0300

    adiciona o construtor em NovaBiometriaRequest

--- src/main/java/br/com/zupacademy/henriquecesar/propostas/dto/request/NovaBiometriaRequest.java
+++ src/main/java/br/com/zupacademy/henriquecesar/propostas/dto/request/NovaBiometriaRequest.java
@@ -2,15 +2,23 @@ package br.com.zupacademy.henriquecesar.propostas.dto.request;
 
 import javax.validation.constraints.NotBlank;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 import br.com.zupacademy.henriquecesar.propostas.common.annotation.Base64;
 
 public class NovaBiometriaRequest {
-	
+
 	@NotBlank
 	@Base64
 	private String fingerPrint;
 
+	@JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
+	public NovaBiometriaRequest(@NotBlank @Base64 String fingerPrint) {
+		this.fingerPrint = fingerPrint;
+	}
+
 	public String getFingerPrint() {
 		return fingerPrint;
 	}
+
 }
