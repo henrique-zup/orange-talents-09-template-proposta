5516e1faa027fe8f8f54fa9590b726d08a3a4da2
Henrique César <henrique.jose@zup.com.br>
2021-11-01 17:35:16 -0300

    adição do endpoint de bloqueio de cartão

--- src/main/java/br/com/zupacademy/henriquecesar/propostas/controller/CartaoController.java
+++ src/main/java/br/com/zupacademy/henriquecesar/propostas/controller/CartaoController.java
@@ -4,6 +4,8 @@ import java.net.URI;
 import java.util.Map;
 import java.util.UUID;
 
+import javax.persistence.EntityManager;
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 import org.springframework.http.ResponseEntity;
@@ -15,19 +17,24 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import br.com.zupacademy.henriquecesar.propostas.dto.request.NovaBiometriaRequest;
+import br.com.zupacademy.henriquecesar.propostas.exception.business.CartaoBloqueadoException;
 import br.com.zupacademy.henriquecesar.propostas.exception.business.CartaoNaoEncontradoException;
 import br.com.zupacademy.henriquecesar.propostas.modelo.Biometria;
+import br.com.zupacademy.henriquecesar.propostas.modelo.BloqueioCartao;
 import br.com.zupacademy.henriquecesar.propostas.modelo.Cartao;
 import br.com.zupacademy.henriquecesar.propostas.repository.CartaoRepository;
+
 
 @RestController
 @RequestMapping("/cartoes")
 public class CartaoController {
 
 	private CartaoRepository cartaoRepository;
+	private EntityManager entityManager;
 
-	public CartaoController(CartaoRepository cartaoRepository) {
+	public CartaoController(CartaoRepository cartaoRepository, EntityManager entityManager) {
 		this.cartaoRepository = cartaoRepository;
+		this.entityManager = entityManager;
 	}
 
 	@PostMapping("/{idCartao}/adicionarBiometria")
@@ -46,6 +53,23 @@ public class CartaoController {
 			.toUri();
 		
 		return ResponseEntity.created(location).build();
+	}
+	
+	@PostMapping("/{idCartao}/bloquear")
+	public void bloquearCartao(@PathVariable UUID idCartao, HttpServletRequest request) {
+		Cartao cartao = cartaoRepository.findById(idCartao)
+				.orElseThrow(CartaoNaoEncontradoException::new);
+		
+		if (cartao.isBloqueado(entityManager)) {
+			throw new CartaoBloqueadoException();
+		}
+		
+		String userAgent = request.getHeader("User-Agent");
+		String enderecoIp = request.getRemoteAddr();
+		
+		BloqueioCartao novoBloqueio = new BloqueioCartao(userAgent, enderecoIp, cartao);
+		
+		cartao.bloquear(novoBloqueio, cartaoRepository);
 	}
 
 }
