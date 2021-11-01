362a6ff1e625c1c1822b506208ff8c9c07fb4c56
Henrique César <henrique.jose@zup.com.br>
2021-11-01 17:30:50 -0300

    criação da entidade BloqueioCartao

---
+++ src/main/java/br/com/zupacademy/henriquecesar/propostas/modelo/BloqueioCartao.java
@@ -0,0 +1,53 @@
+package br.com.zupacademy.henriquecesar.propostas.modelo;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+@Entity
+public class BloqueioCartao {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank
+	private String userAgent;
+	
+	@NotBlank
+	private String enderecoIp;
+	
+	@NotNull
+	@ManyToOne(optional = false)
+	private Cartao cartao;
+	
+	@CreationTimestamp
+	private LocalDateTime dataCadastro;
+	
+	@NotNull
+	private boolean ativo;
+
+	@Deprecated
+	public BloqueioCartao() {
+	}
+
+	public BloqueioCartao(@NotBlank String userAgent, @NotBlank String enderecoIp, @NotNull Cartao cartao) {
+		this.userAgent = userAgent;
+		this.enderecoIp = enderecoIp;
+		this.cartao = cartao;
+		this.ativo = true;
+	}
+
+	public boolean isAtivo() {
+		return ativo;
+	}
+}
+
