1077f2c1929231e830c933dcde0fd8a0ddb3f484
Henrique César <henrique.jose@zup.com.br>
2021-11-05 17:25:32 -0300

    adicionar annotation @Enumerated no enum da carteira digital

--- src/main/java/br/com/zupacademy/henriquecesar/propostas/modelo/CarteiraDigital.java
+++ src/main/java/br/com/zupacademy/henriquecesar/propostas/modelo/CarteiraDigital.java
@@ -1,6 +1,8 @@
 package br.com.zupacademy.henriquecesar.propostas.modelo;
 
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -15,6 +17,7 @@ public class CarteiraDigital {
 	private Long id;
 	
 	@NotNull
+	@Enumerated(EnumType.STRING)
 	private CarteiraDigitalServico servico;
 	
 	@ManyToOne
